32b6f751ef1d6fe76239be69e08e799fdba01a52
Unknown <timothee.monceaux@gmail.com>
2018-07-22 11:35:39 +0200

    Added run.sh script to run the front

---
+++ run.sh
@@ -0,0 +1,8 @@
+# If not run externally, we need to launch the server (on port 3000)
+#npm start
+
+# To run the front, use the following command
+chromium-browser --app=http://localhost:3000 --start-fullscreen
+
+# Terminate
+#pkill chromium
