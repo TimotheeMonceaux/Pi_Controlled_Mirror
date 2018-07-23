e2a67d6c170f80f07d37a68c77fdd3ee001c7ba1
Unknown <timothee.monceaux@gmail.com>
2018-07-23 22:49:20 +0200

    First draw of the Clock component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Clock from './clock/clock.js';
 import XKCD from './xkcd/xkcd.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Clock />
         <XKCD />
       </div>
     );
