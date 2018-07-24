9628787337a0eaef2ab19450684211c910119308
Unknown <timothee.monceaux@gmail.com>
2018-07-24 22:55:22 +0200

    Meteo component is now fully working. Need some CSS though

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import Clock from './clock/clock.js';
 import XKCD from './xkcd/xkcd.js';
+import Meteo from './meteo/meteo.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Clock />
+        <Meteo />
         <XKCD />
       </div>
     );
---
+++ src/meteo/meteo.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import config from './config.json';
+
+class Meteo extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loaded: false,
+            name: "",
+            description: "",
+            icon: [],
+            temp: "",
+            min: "",
+            max: ""
+        }
+    }
+
+    componentDidMount() {
+        this.refresh();
+        this.intervalID = setInterval(
+            () => this.refresh(),
+            1000 * 3600
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.intervalID);
+    }
+
+    refresh() {
+        fetch('http://api.openweathermap.org/data/2.5/weather?id='+config.cityid+'&units=metric&lang=fr&appid='+config.apikey)
+        .then(response => response.json())
+        .then(data => this.setState({
+            loaded: true,
+            name: data.name,
+            description: data.weather[0].description,
+            icon: [<img src={"http://openweathermap.org/img/w/"+data.weather[0].icon+".png"} alt="Weather Icon" />],
+            temp: data.main.temp,
+            min: data.main.temp_min,
+            max: data.main.temp_max
+        }))
+    }
+
+    render() {
+        if (!this.state.loaded)
+            return(<p>Loading...</p>);
+        return (<div className="meteo">{this.state.icon} {this.state.name}: {this.state.description} - {this.state.temp} ({this.state.min} - {this.state.max})</div>)
+    }
+}
+
+export default Meteo;
